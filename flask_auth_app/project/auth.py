087e9f45ea9ac38f1a9923cb4815a13aa5821f6f
diawando <abduldiawade777@gmail.com>
2023-03-22 23:08:58 +0000

    ajout du controle de l'existence de l'utilisateur

--- flask_auth_app/project/auth.py
+++ flask_auth_app/project/auth.py
@@ -3,6 +3,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
 from . import create_app
+
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
@@ -12,6 +14,19 @@ def login():
 @auth.route('/login', methods=['POST'])
 def login_post():
     # login code goes here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    
+    user = User.query.filter_by(email=email).first()
+    
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+    
+    # if the above check passes, then we know the user has the right credentials
     return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
